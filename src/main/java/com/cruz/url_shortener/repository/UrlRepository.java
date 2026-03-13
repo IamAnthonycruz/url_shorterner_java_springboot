7bc54da6d02fc7996d8247a50f60d4f14dfc17de
IamAnthonyCruz <Charliecruz1224@gmail.com>
2026-03-13 11:37:20 -0400

    Add cache invalidation

--- src/main/java/com/cruz/url_shortener/repository/UrlRepository.java
+++ src/main/java/com/cruz/url_shortener/repository/UrlRepository.java
@@ -1,11 +1,19 @@
 package com.cruz.url_shortener.repository;
 
 import com.cruz.url_shortener.entity.Url;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByLongUrl(String longUrl);
     Optional<Url> findByShortCode(String shortCode);
+    @Transactional
+    @Modifying
+    @Query("Update Url u SET u.hitCount = u.hitCount+ 1 WHERE u.shortCode = :shortCode")
+    void incrementHitCount(@Param("shortCode") String shortCode);
 }
